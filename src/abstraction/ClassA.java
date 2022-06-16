5150b1ecb30ed5d0038ecce944a27ff686aed910
unknown <you@example.com>
2022-06-16 17:26:14 +0530

    global

--- src/abstraction/ClassA.java
+++ src/abstraction/ClassA.java
@@ -5,10 +5,10 @@ public abstract class ClassA {
 	abstract public void m1();
 	abstract public void m2();
 	
-	
+	  int a=10; int b=10;
 	public void m3()
 	{
-		System.out.println("velocity");
+		System.out.println("   velocity");
 	}
 	
 	public static void m4()
